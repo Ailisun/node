e90181a4380192f6e9bfcd8ee73169b3f076563b
Ailisun <13prichin@gmail.com>
2022-06-15 01:57:58 +0300

    part 1. rename, sortFolders, path/promises

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-const path = require('path')
+// const fs = require('fs')
+// const path = require('path')
 
 //Дублюється код, нижче буде динамічний варіант
 // const sortBoys = () => {
@@ -57,29 +57,54 @@ const path = require('path')
 // sortGirls()
 
 //Варіант 2
-const sortFiles = (read,gender, location) => {
-    fs.readdir(path.join(__dirname, read), (err, files) => {
-        if (err) {
-            return console.log(err)
+// const sortFiles = (read,gender, location) => {
+//     fs.readdir(path.join(__dirname, read), (err, files) => {
+//         if (err) {
+//             return console.log(err)
+//         }
+//         files.forEach((file) => {
+//             const readFolderPath = path.join(__dirname, read, file)
+//
+//             fs.readFile(readFolderPath, (err1, data) => {
+//                 if (err1) return console.log(err1)
+//
+//                 const user = JSON.parse(data.toString())
+//                 if (user.gender === gender){
+//                     fs.rename(readFolderPath, path.join(__dirname, location, file), (err) => {
+//                         if (err) return console.log(err)
+//
+//                     })
+//                 }
+//
+//             })
+//         })
+//     })
+// }
+//
+// sortFiles('girls', 'male', 'boys')
+// sortFiles('boys', 'female', 'girls')
+
+//Варіант 3 через проміси
+const fs = require('fs/promises');
+const path = require('path');
+
+const sortFolder = async (read, gender, location) => {
+    try {
+        const files = await fs.readdir(path.join(__dirname, read));
+
+        for (const file of files) {
+            const redFolderPath = path.join(__dirname, read, file);
+            const data = await fs.readFile(redFolderPath);
+            const user = JSON.parse(data.toString());
+
+            if (user.gender === gender) {
+                await fs.rename(redFolderPath, path.join(__dirname, location, file));
+            }
         }
-        files.forEach((file) => {
-            const readFolderPath = path.join(__dirname, read, file)
-
-            fs.readFile(readFolderPath, (err1, data) => {
-                if (err1) return console.log(err1)
-
-                const user = JSON.parse(data.toString())
-                if (user.gender === gender){
-                    fs.rename(readFolderPath, path.join(__dirname, location, file), (err) => {
-                        if (err) return console.log(err)
-
-                    })
-                }
-
-            })
-        })
-    })
+    }catch (e) {
+        console.error(e);
+    }
 }
 
-sortFiles('girls', 'male', 'boys')
-sortFiles('boys', 'female', 'girls')
+sortFolder('girls', 'male', 'boys');
+sortFolder('boys', 'female', 'girls');
